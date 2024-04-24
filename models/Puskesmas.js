bac9c4be32a53b1c5a24a3d364f7a97cf517ff16
M. Adit Prinansyah <maditprinansyah@gmail.com>
2024-04-24 18:45:50 +0700

    Add files via upload

---
+++ models/Puskesmas.js
@@ -0,0 +1,62 @@
+module.exports = (sequelize, DataTypes) => {
+  const Puskesmas = sequelize.define(
+    'Puskesmas',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+      },
+      pic: {
+        type: DataTypes.STRING,
+      },
+      phone: {
+        type: DataTypes.STRING,
+      },
+      tot_posyandu: {
+        type: DataTypes.STRING,
+      },
+      email: {
+        type: DataTypes.STRING,
+      },
+      province: {
+        type: DataTypes.STRING,
+      },
+      city: {
+        type: DataTypes.STRING,
+      },
+      district: {
+        type: DataTypes.STRING,
+      },
+      village: {
+        type: DataTypes.STRING,
+      },
+      address: {
+        type: DataTypes.TEXT,
+      },
+      latitude: {
+        type: DataTypes.STRING,
+      },
+      longitude: {
+        type: DataTypes.STRING,
+      },
+      postal_code: {
+        type: DataTypes.STRING,
+      },
+    },
+    { tableName: 'puskesmas' },
+  );
+
+  //hasmany children vaccinations
+  Puskesmas.associate = (models) => {
+    Puskesmas.hasMany(models.Nakes, {
+      foreignKey: 'puskesmas_id',
+      as: 'nakes',
+    });
+  };
+
+  return Puskesmas;
+};
